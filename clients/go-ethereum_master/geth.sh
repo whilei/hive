7a662be69aeacb1bcfb79057da3045b914230c18
Frank Szendzielarz <Frank@franks.technology>
2018-12-18 19:28:05 +0100

    Basic syncs done

--- clients/go-ethereum_master/geth.sh
+++ clients/go-ethereum_master/geth.sh
@@ -51,10 +51,10 @@ fi
 
 # Handle any client mode or operation requests
 if [ "$HIVE_NODETYPE" == "full" ]; then
-	FLAGS="$FLAGS --fast"
+	FLAGS="$FLAGS --syncmode fast "
 fi
 if [ "$HIVE_NODETYPE" == "light" ]; then
-	FLAGS="$FLAGS --light"
+	FLAGS="$FLAGS --syncmode light "
 fi
 
 if [ "$HIVE_USE_GENESIS_CONFIG" == ""]; then
